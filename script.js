0f3c271a276c9a1db9567be86b945d050124836d
rsgand5k <rsgand5k@gmail.com>
2026-02-11 13:10:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -191,6 +191,7 @@ const runs5kRsg = [
   { name:"LudovikMC", date:"2025-11-23", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:48,s:1,ms:62}, rta:{h:0,m:49,s:2,ms:164}, k5:{h:0,m:19,s:57,ms:0}, seed:"9209975185873643422", link:"" },
   { name:"LudovikMC", date:"2025-11-26", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:37,s:30,ms:681}, rta:{h:0,m:38,s:19,ms:254}, k5:{h:0,m:26,s:34,ms:0}, seed:"-8377709639459139130", link:"" },
   { name:"LudovikMC", date:"2025-12-27", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:42,s:39,ms:821}, rta:{h:0,m:42,s:54,ms:017}, k5:{h:0,m:18,s:22,ms:900}, seed:"3852899728699119638", link:"https://www.twitch.tv/videos/2654352051" },
+	{ name:"LudovikMC", date:"2026-02-11", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:28,s:30,ms:893}, rta:{h:0,m:29,s:47,ms:938}, k5:{h:0,m:18,s:31,ms:0}, seed:"471589420443183072", link:"https://www.twitch.tv/videos/2694879300" },
 ];
 
 function applyFilter() {
