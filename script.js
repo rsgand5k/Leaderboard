d488e10c066fbba892140f1eee667cb46aba51df
rsgand5k <rsgand5k@gmail.com>
2026-04-05 12:16:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let pxPerMs = 0.0003;
 let timeMode = "igt";
+let ratio_screen = 70;
 
 function countryToFlag(code) {
   return code.toUpperCase().replace(/./g, c =>
@@ -19,30 +20,25 @@ function formatTime(ms) {
   return `${m}m ${s}s ${ms}ms`;
 }
 
-function filterBestRuns(list) {
+function filterBestRuns(list, firstKey, secondKey) {
   const best = new Map();
+
   list.forEach(run => {
-    const total = parseTime(run[timeMode]) + parseTime(run.k5);
-    if (!best.has(run.name) || total < best.get(run.name).total)
+    const total = getTimeValue(run, firstKey) + getTimeValue(run, secondKey);
+
+    if (!best.has(run.name) || total < best.get(run.name).total) {
       best.set(run.name, { ...run, total });
+    }
   });
+
   return [...best.values()];
 }
 
-function sortByTotalTime(list) {
+function sortByTotalTime(list, firstKey, secondKey) {
   return list.slice().sort((a,b) =>
-    (parseTime(a[timeMode]) + parseTime(a.k5)) -
-    (parseTime(b[timeMode]) + parseTime(b.k5))
+    (getTimeValue(a, firstKey) + getTimeValue(a, secondKey)) -
+    (getTimeValue(b, firstKey) + getTimeValue(b, secondKey))
   );
-}
-
-function updatePxPerMs(l1, l2) {
-  const all = [...l1, ...l2];
-  if (!all.length) return;
-  const maxTotal = Math.max(...all.map(r =>
-    parseTime(r[timeMode]) + parseTime(r.k5)
-  ));
-  pxPerMs = (window.innerWidth * 0.65) / maxTotal;
 }
 
 function filterF3(list) {
@@ -50,9 +46,7 @@ function filterF3(list) {
   const showNoF3 = document.getElementById("showNoF3").checked;
 
   return list.filter(run => {
-    const hasF3 = (typeof run.F3 !== 'undefined') ? run.F3 : run.f3;
-    const isF3 = Boolean(hasF3);
-
+    const isF3 = Boolean(run.f3 ?? run.F3);
     if (isF3 && showF3) return true;
     if (!isF3 && showNoF3) return true;
     return false;
@@ -60,31 +54,42 @@ function filterF3(list) {
 }
 
 function getSuffix(n) {
-    if (n === 1) return "st";
-    if (n === 2) return "nd";
-    if (n === 3) return "rd";
-    return "th";
+  if (n === 1) return "st";
+  if (n === 2) return "nd";
+  if (n === 3) return "rd";
+  return "th";
 }
 
-function renderBoard(list, containerId, firstKey, secondKey) {
+function updatePxPerMsForBoard(board) {
+  if (!board.list || board.list.length === 0) return;
+
+  const maxTotal = Math.max(...board.list.map(r =>
+    getTimeValue(r, board.first) + getTimeValue(r, board.second)
+  ));
+
+  if (maxTotal === 0) return 0;
+
+  return (window.innerWidth * (ratio_screen / 100)) / maxTotal;
+}
+
+function renderBoard(list, containerId, firstKey, secondKey, localPxPerMs) {
   const board = document.getElementById(containerId);
+  if (!board) return;
+
   board.querySelectorAll(".row").forEach(e => e.remove());
 
   list.forEach((p, index) => {
-    const tRsg = parseTime(p[timeMode]);
-    const t5k = parseTime(p.k5);
-
-    const part1 = (firstKey === "rsg") ? tRsg : t5k;
-    const part2 = (secondKey === "rsg") ? tRsg : t5k;
-
+    const part1 = getTimeValue(p, firstKey);
+    const part2 = getTimeValue(p, secondKey);
     const total = part1 + part2;
+    if (total === 0) return;
 
     const row = document.createElement("div");
     row.className = "row";
     row.style.display = "flex";
     row.style.position = "relative";
-
-    row.addEventListener("mouseenter", () => {
+	
+	row.addEventListener("mouseenter", () => {
 
     const nameSpan = row.querySelector(".name");
     const rect = nameSpan.getBoundingClientRect();
@@ -109,14 +114,11 @@ function renderBoard(list, containerId, firstKey, secondKey) {
     `, x, y);
 	});
 
-    // Rank
     const rank = document.createElement("div");
     rank.style.width = "40px";
     rank.style.fontWeight = "bold";
     rank.style.marginRight = "10px";
     rank.textContent = (index+1) + getSuffix(index+1);
-
-    const f3Txt = p.f3 ? "F3" : "No F3";
 
     const info = document.createElement("div");
     info.className = "info-row";
@@ -124,23 +126,21 @@ function renderBoard(list, containerId, firstKey, secondKey) {
       <span class="name">${p.name}</span>
       <span>${countryToFlag(p.country)}</span>
       <span>${p.version}</span>
-      <span>${f3Txt}</span>
+      <span>${p.f3 ? "F3" : "No F3"}</span>
       <span>${p.difficulty}</span>
     `;
 
     const bar = document.createElement("div");
     bar.className = "bar-container";
-    bar.style.width = (total * pxPerMs) + "px";
+    bar.style.width = (total * localPxPerMs) + "px";
 
-    // 1st bar
     const b1 = document.createElement("div");
-    b1.className = (firstKey === "rsg") ? "igt" : "k5";
+    b1.className = firstKey === "main" ? "igt" : "time";
     b1.style.width = (part1 / total * 100) + "%";
     b1.textContent = formatTime(part1);
 
-    // 2nd bar
     const b2 = document.createElement("div");
-    b2.className = (secondKey === "rsg") ? "igt" : "k5";
+    b2.className = secondKey === "main" ? "igt" : "time";
     b2.style.width = (part2 / total * 100) + "%";
     b2.textContent = formatTime(part2);
 
@@ -148,22 +148,20 @@ function renderBoard(list, containerId, firstKey, secondKey) {
     bar.appendChild(b2);
 
     const totalTxt = document.createElement("div");
-    totalTxt.style.marginLeft = "10px";
+    totalTxt.className = "total-time";
     totalTxt.textContent = formatTime(total);
 
     const barRow = document.createElement("div");
-	barRow.className = "run";
-	barRow.style.display = "flex";
-	barRow.style.alignItems = "center";
+    barRow.className = "run";
+    barRow.style.display = "flex";
+    barRow.style.alignItems = "center";
 
-	const barWrapper = document.createElement("div");
-	barWrapper.style.flex = "1";
-	barWrapper.appendChild(bar);
+    const barWrapper = document.createElement("div");
+    barWrapper.style.flex = "1";
+    barWrapper.appendChild(bar);
 
-	totalTxt.className = "total-time";
-
-	barRow.appendChild(barWrapper);
-	barRow.appendChild(totalTxt);
+    barRow.appendChild(barWrapper);
+    barRow.appendChild(totalTxt);
 
     const col = document.createElement("div");
     col.style.flex = "1";
@@ -177,57 +175,94 @@ function renderBoard(list, containerId, firstKey, secondKey) {
   });
 }
 
-const runsRsg5k = [
-  { name:"Antoine", date:"2025-11-23", version:"1.16.1", f3:true, country:"FR", mods:"Modded", difficulty:"Easy", igt:{h:0,m:18,s:55,ms:394}, rta:{h:0,m:19,s:38,ms:397}, k5:{h:0,m:31,s:24,ms:0}, seed:"?", link:"https://www.twitch.tv/videos/2625908254" },
-  { name:"LudovikMC", date:"2025-11-27", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:25,s:5,ms:576}, rta:{h:0,m:25,s:39,ms:714}, k5:{h:0,m:22,s:13,ms:0}, seed:"-5068990900990481486", link:"" },
-  { name:"DesktopFolder", date:"2025-11-29", version:"1.16.1", f3:true, country:"CA", mods:"Modded", difficulty:"Easy", igt:{h:0,m:16,s:10,ms:699}, rta:{h:0,m:16,s:36,ms:958}, k5:{h:0,m:22,s:44,ms:150}, seed:"4460252521909011407", link:"https://www.youtube.com/watch?v=nizPV0YUZ4Q" },
-  { name:"Blyde", date:"2025-11-30", version:"1.16.1", f3:true, country:"FR", mods:"Modded", difficulty:"Easy", igt:{h:0,m:19,s:57,ms:71}, rta:{h:0,m:20,s:42,ms:53}, k5:{h:0,m:22,s:55,ms:0}, seed:"5528818920531833096", link:"" },
-  { name:"Fr4nkey", date:"2025-12-02", version:"1.16.1", f3:true, country:"RU", mods:"Modded", difficulty:"Easy", igt:{h:0,m:13,s:57,ms:820}, rta:{h:0,m:14,s:15,ms:0}, k5:{h:0,m:25,s:17,ms:0}, seed:"8929439908023461646", link:"https://www.youtube.com/watch?v=PjCsG_eJcKA" },
-  { name:"Fr4nkey", date:"2025-12-13", version:"1.16.1", f3:true, country:"RU", mods:"Modded", difficulty:"Easy", igt:{h:0,m:11,s:20,ms:327}, rta:{h:0,m:11,s:31,ms:0}, k5:{h:0,m:22,s:14,ms:240}, seed:"6869386029071364720", link:"https://www.youtube.com/watch?v=GoI8jzo3C6Y" },
-  { name:"LudovikMC", date:"2026-01-17", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:17,s:17,ms:235}, rta:{h:0,m:18,s:06,ms:272}, k5:{h:0,m:19,s:34,ms:500}, seed:"-1026921766230817449", link:"https://www.youtube.com/watch?v=M3GxF49uBFA" },
-{ name:"Legoboy1234", date:"2026-03-31", version:"1.16.1", f3:true, country:"US", mods:"Modded", difficulty:"Easy", igt:{h:0,m:11,s:48,ms:717}, rta:{h:0,m:12,s:04,ms:875}, k5:{h:0,m:28,s:12,ms:0}, seed:"5211054053007582760", link:"https://youtu.be/ICxFfBU9yA8" },
+const runs_any_5K = [
+  { name:"Antoine", date:"2025-11-23", version:"1.16.1", f3:true, country:"FR", mods:"Modded", difficulty:"Easy", igt:{h:0,m:18,s:55,ms:394}, rta:{h:0,m:19,s:38,ms:397}, time:{h:0,m:31,s:24,ms:0}, seed:"?", link:"https://www.twitch.tv/videos/2625908254" },
+  { name:"LudovikMC", date:"2025-11-27", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:25,s:5,ms:576}, rta:{h:0,m:25,s:39,ms:714}, time:{h:0,m:22,s:13,ms:0}, seed:"-5068990900990481486", link:"" },
+  { name:"DesktopFolder", date:"2025-11-29", version:"1.16.1", f3:true, country:"CA", mods:"Modded", difficulty:"Easy", igt:{h:0,m:16,s:10,ms:699}, rta:{h:0,m:16,s:36,ms:958}, time:{h:0,m:22,s:44,ms:150}, seed:"4460252521909011407", link:"https://www.youtube.com/watch?v=nizPV0YUZ4Q" },
+  { name:"Blyde", date:"2025-11-30", version:"1.16.1", f3:true, country:"FR", mods:"Modded", difficulty:"Easy", igt:{h:0,m:19,s:57,ms:71}, rta:{h:0,m:20,s:42,ms:53}, time:{h:0,m:22,s:55,ms:0}, seed:"5528818920531833096", link:"" },
+  { name:"Fr4nkey", date:"2025-12-02", version:"1.16.1", f3:true, country:"RU", mods:"Modded", difficulty:"Easy", igt:{h:0,m:13,s:57,ms:820}, rta:{h:0,m:14,s:15,ms:0}, time:{h:0,m:25,s:17,ms:0}, seed:"8929439908023461646", link:"https://www.youtube.com/watch?v=PjCsG_eJcKA" },
+  { name:"Fr4nkey", date:"2025-12-13", version:"1.16.1", f3:true, country:"RU", mods:"Modded", difficulty:"Easy", igt:{h:0,m:11,s:20,ms:327}, rta:{h:0,m:11,s:31,ms:0}, time:{h:0,m:22,s:14,ms:240}, seed:"6869386029071364720", link:"https://www.youtube.com/watch?v=GoI8jzo3C6Y" },
+  { name:"LudovikMC", date:"2026-01-17", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:17,s:17,ms:235}, rta:{h:0,m:18,s:06,ms:272}, time:{h:0,m:19,s:34,ms:500}, seed:"-1026921766230817449", link:"https://www.youtube.com/watch?v=M3GxF49uBFA" },
+  { name:"Legoboy1234", date:"2026-03-31", version:"1.16.1", f3:true, country:"US", mods:"Modded", difficulty:"Easy", igt:{h:0,m:11,s:48,ms:717}, rta:{h:0,m:12,s:04,ms:875}, time:{h:0,m:28,s:12,ms:0}, seed:"5211054053007582760", link:"https://youtu.be/ICxFfBU9yA8" },
 ];
 
-const runs5kRsg = [
-  { name:"LudovikMC", date:"2025-11-23", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:48,s:1,ms:62}, rta:{h:0,m:49,s:2,ms:164}, k5:{h:0,m:19,s:57,ms:0}, seed:"9209975185873643422", link:"" },
-  { name:"LudovikMC", date:"2025-11-26", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:37,s:30,ms:681}, rta:{h:0,m:38,s:19,ms:254}, k5:{h:0,m:26,s:34,ms:0}, seed:"-8377709639459139130", link:"" },
-  { name:"LudovikMC", date:"2025-12-27", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:42,s:39,ms:821}, rta:{h:0,m:42,s:54,ms:017}, k5:{h:0,m:18,s:22,ms:900}, seed:"3852899728699119638", link:"https://www.twitch.tv/videos/2654352051" },
-  { name:"LudovikMC", date:"2026-02-11", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:28,s:30,ms:893}, rta:{h:0,m:29,s:47,ms:938}, k5:{h:0,m:18,s:31,ms:0}, seed:"471589420443183072", link:"https://www.twitch.tv/videos/2694879300" },
-  { name:"DesktopFolder", date:"2026-03-05", version:"1.16.1", f3:true, country:"CA", mods:"Modded", difficulty:"Easy", igt:{h:0,m:21,s:57,ms:073}, rta:{h:0,m:22,s:42,ms:551}, k5:{h:0,m:23,s:47,ms:0}, seed:"-6113485235216793906", link:"https://www.youtube.com/watch?v=ImcDBA5ZppE" },
-  { name:"Doog4321", date:"2026-03-10", version:"1.16.1", f3:true, country:"US", mods:"Modded", difficulty:"Easy", igt:{h:0,m:23,s:53,ms:027}, rta:{h:0,m:25,s:19,ms:0}, k5:{h:0,m:26,s:41,ms:0}, seed:"5179192011070350515", link:"https://www.youtube.com/live/HATy5dF9na0?t=446s" },
+const runs_5K_any = [
+  { name:"LudovikMC", date:"2025-11-23", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:48,s:1,ms:62}, rta:{h:0,m:49,s:2,ms:164}, time:{h:0,m:19,s:57,ms:0}, seed:"9209975185873643422", link:"" },
+  { name:"LudovikMC", date:"2025-11-26", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:37,s:30,ms:681}, rta:{h:0,m:38,s:19,ms:254}, time:{h:0,m:26,s:34,ms:0}, seed:"-8377709639459139130", link:"" },
+  { name:"LudovikMC", date:"2025-12-27", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hard", igt:{h:0,m:42,s:39,ms:821}, rta:{h:0,m:42,s:54,ms:017}, time:{h:0,m:18,s:22,ms:900}, seed:"3852899728699119638", link:"https://www.twitch.tv/videos/2654352051" },
+  { name:"LudovikMC", date:"2026-02-11", version:"1.16.1", f3:false, country:"FR", mods:"Modded", difficulty:"Hardcore", igt:{h:0,m:28,s:30,ms:893}, rta:{h:0,m:29,s:47,ms:938}, time:{h:0,m:18,s:31,ms:0}, seed:"471589420443183072", link:"https://www.twitch.tv/videos/2694879300" },
+  { name:"DesktopFolder", date:"2026-03-05", version:"1.16.1", f3:true, country:"CA", mods:"Modded", difficulty:"Easy", igt:{h:0,m:21,s:57,ms:073}, rta:{h:0,m:22,s:42,ms:551}, time:{h:0,m:23,s:47,ms:0}, seed:"-6113485235216793906", link:"https://www.youtube.com/watch?v=ImcDBA5ZppE" },
+  { name:"Doog4321", date:"2026-03-10", version:"1.16.1", f3:true, country:"US", mods:"Modded", difficulty:"Easy", igt:{h:0,m:23,s:53,ms:027}, rta:{h:0,m:25,s:19,ms:0}, time:{h:0,m:26,s:41,ms:0}, seed:"5179192011070350515", link:"https://www.youtube.com/live/HATy5dF9na0?t=446s" },
 ];
+
+const runs_half_10K = [];
+const runs_10K_half = [
+  { name:"DesktopFolder", date:"2026-04-04", version:"1.16.1", f3:true, country:"CA", mods:"Modded", difficulty:"Easy", igt:{h:0,m:45,s:51,ms:227}, rta:{h:0,m:49,s:53,ms:915}, time:{h:0,m:51,s:01,ms:0}, seed:"-7226257513615693532", link:"https://www.youtube.com/watch?v=FUJaNkovjZQ" },
+];
+
+const runs_hdwgh_21K = [];
+const runs_21K_hdwgh = [];
+
+const runs_aa_42K = [];
+const runs_42K_aa = [];
+
+const allBoards = [
+  { data: runs_any_5K, id: "any-5K", first: "main", second: "time" },
+  { data: runs_5K_any, id: "5K-any", first: "time", second: "main" },
+
+  { data: runs_half_10K, id: "half-10K", first: "main", second: "time" },
+  { data: runs_10K_half, id: "10K-half", first: "time", second: "main" },
+
+  { data: runs_hdwgh_21K, id: "hdwgh-21K", first: "main", second: "time" },
+  { data: runs_21K_hdwgh, id: "21K-hdwgh", first: "time", second: "main" },
+
+  { data: runs_aa_42K, id: "aa-42k", first: "main", second: "time" },
+  { data: runs_42K_aa, id: "42K-aa", first: "time", second: "main" }
+];
+
+function getTimeValue(run, key) {
+  let t = null;
+
+  if (key === "main") t = run[timeMode];
+  if (key === "time") t = run.time;
+
+  if (!t) return 0;
+
+  return parseTime(t);
+}
 
 function applyFilter() {
-  const date = document.getElementById('dateFilter').value;
-  const topX = Number(document.getElementById('topX').value || 10);
-  const showObsolete = document.getElementById('showObsolete').checked;
+  const date = document.getElementById('dateFilter')?.value;
+  const topX = Number(document.getElementById('topX')?.value || 10);
+  const showObsolete = document.getElementById('showObsolete')?.checked;
 
-  let f1 = runsRsg5k.filter(r => !date || r.date <= date);
-  let f2 = runs5kRsg.filter(r => !date || r.date <= date);
+  allBoards.forEach(board => {
+    let list = board.data;
 
-  if (!showObsolete) {
-    f1 = filterBestRuns(f1);
-    f2 = filterBestRuns(f2);
-  }
+    list = list.filter(r => !date || r.date <= date);
 
-  f1 = filterF3(f1);
-  f2 = filterF3(f2);
+    if (!showObsolete) {
+      list = filterBestRuns(list, board.first, board.second);
+    }
 
-  f1 = sortByTotalTime(f1).slice(0, topX);
-  f2 = sortByTotalTime(f2).slice(0, topX);
+    list = filterF3(list);
 
-  updatePxPerMs(f1, f2);
+    list = sortByTotalTime(list, board.first, board.second).slice(0, topX);
 
-  renderBoard(f1, "leftBoard", "rsg", "k5");
-  renderBoard(f2, "rightBoard", "k5", "rsg");
+    board.list = list;
+
+    const localPx = updatePxPerMsForBoard(board);
+
+    renderBoard(list, board.id, board.first, board.second, localPx);
+  });
 }
 
 window.addEventListener("DOMContentLoaded", applyFilter);
 window.addEventListener("resize", applyFilter);
 
-document.getElementById('dateFilter').addEventListener('change', applyFilter);
-document.getElementById('topX').addEventListener('input', applyFilter);
-document.getElementById('showObsolete').addEventListener('change', applyFilter);
+document.getElementById('dateFilter')?.addEventListener('change', applyFilter);
+document.getElementById('topX')?.addEventListener('input', applyFilter);
+document.getElementById('showObsolete')?.addEventListener('change', applyFilter);
 
 document.querySelectorAll('input[name="timeMode"]').forEach(r =>
   r.addEventListener('change', e => {
@@ -236,8 +271,8 @@ document.querySelectorAll('input[name="timeMode"]').forEach(r =>
   })
 );
 
-document.getElementById("showF3").addEventListener("change", applyFilter);
-document.getElementById("showNoF3").addEventListener("change", applyFilter);
+document.getElementById("showF3")?.addEventListener("change", applyFilter);
+document.getElementById("showNoF3")?.addEventListener("change", applyFilter);
 
 const tooltip = document.getElementById("tooltip");
 let tooltipLocked = false;
